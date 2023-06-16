d48799cf05c31714a3bbfb32d7e20b562aa5a426
Olivierish <olivierish@gmail.com>
2023-06-16 16:36:05 +0200

    Kurs 16.06.2023 Database Insert,password - hash - mail - eindeutig, Exercises

---
+++ php/Uebungen/tag15/views/posts.view.php
@@ -0,0 +1,132 @@
+<?php
+// $stmt = $db->prepare('SELECT * FROM `posts` ');
+// $stmt->execute();
+// $posts = $stmt->fetchAll();
+// print_r($posts);
+?>
+
+<header  class="jumbotron bg-image px-5 py-3 position-relative" style="background :linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 70%),url('images/bg3.jpg'); height: calc(100vh - 500px); background-size:100% 160%; background-position:100%; background-repeat: no-repeat;">
+    <p class="display-5 text-light position-absolute top-50">Contact us</p>
+</header>
+
+<div class="bg-main p-5">
+<main>
+    <div class="row">
+            <div class="col-md-6 py-3">
+                <div class="card col-md-12 bg-dark">
+                    <!-- <img class="card-img-top" src="images/cocktails/maracuja.jpg" alt="Card image cap" width="300" height="300"> -->
+                    <img class="card-img-top" src="images/cocktails/maracuja.jpg" alt="Card image cap" style="height: 100%;">
+                    <div class="card-body">
+                    <h5 class="card-title  color-5">Card title</h5>
+                    <p class="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <button type="button" class="btn btn-sm btn-secondary">Read More</button>    
+                </div>
+                    <div class="card-footer ms-auto">
+                    <small class="text-secondary">Maria Panev</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 py-3">
+                <div class="row">
+                <div class="col-md-6">
+                        <div class="card col-12 bg-dark">
+                            <img class="card-img-top " src="images/cocktails/maracuja.jpg" alt="Card image cap" style="height: 100%;">
+                            <div class="card-body">
+                            <h5 class="card-title  color-5">Card title</h5>
+                            <p class="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button type="button" class="btn btn-sm btn-secondary">Read More</button>
+                            </div>
+                            <div class="card-footer ms-auto">
+                            <small class="text-secondary">Paul Walker</small>
+                            </div>
+                        </div>
+                </div>
+            <div class="col-md-6">
+                <div class="card col-12 bg-dark">
+                                <img class="card-img-top" src="images/cocktails/maracuja.jpg" alt="Card image cap" style="height: 100%;">
+                                <div class="card-body">
+                                    <h5 class="card-title color-5">Card title</h5>
+                                    <p class="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <button type="button" class="btn btn-sm btn-secondary">Read More</button>
+                                </div>
+                                <div class="card-footer ms-auto">
+                                    <small class="text-secondary">Miri Müller</small>
+                                </div>
+                </div>
+            </div>
+            
+        </div>
+            
+    </div>
+    <!-- g -->
+    <div class="col-md-6 py-3">
+                <div class="row">
+                <div class="col-md-6">
+                        <div class="card col-12 bg-dark">
+                            <img class="card-img-top " src="images/cocktails/maracuja.jpg" alt="Card image cap" style="height: 100%;">
+                            <div class="card-body">
+                            <h5 class="card-title  color-5">Card title</h5>
+                            <p class="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button type="button" class="btn btn-sm btn-secondary">Read More</button>
+                            </div>
+                            <div class="card-footer ms-auto">
+                            <small class="text-secondary">Paul Walker</small>
+                            </div>
+                        </div>
+                </div>
+            <div class="col-md-6">
+                <div class="card col-12 bg-dark">
+                                <img class="card-img-top" src="images/cocktails/maracuja.jpg" alt="Card image cap" style="height: 100%;">
+                                <div class="card-body">
+                                    <h5 class="card-title color-5">Card title</h5>
+                                    <p class="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <button type="button" class="btn btn-sm btn-secondary">Read More</button>
+                                </div>
+                                <div class="card-footer ms-auto">
+                                    <small class="text-secondary">Miri Müller</small>
+                                </div>
+                </div>
+            </div>
+            
+        </div>
+            
+    </div>
+    <div class="col-md-6 py-3">
+                <div class="row">
+                <div class="col-md-6">
+                        <div class="card col-12 bg-dark">
+                            <img class="card-img-top " src="images/cocktails/maracuja.jpg" alt="Card image cap" style="height: 100%;">
+                            <div class="card-body">
+                            <h5 class="card-title  color-5">Card title</h5>
+                            <p class="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button type="button" class="btn btn-sm btn-secondary">Read More</button>
+                            </div>
+                            <div class="card-footer ms-auto">
+                            <small class="text-secondary">Paul Walker</small>
+                            </div>
+                        </div>
+                </div>
+            <div class="col-md-6">
+                <div class="card col-12 bg-dark">
+                                <img class="card-img-top" src="images/cocktails/maracuja.jpg" alt="Card image cap" style="height: 100%;">
+                                <div class="card-body">
+                                    <h5 class="card-title color-5">Card title</h5>
+                                    <p class="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <button type="button" class="btn btn-sm btn-secondary">Read More</button>
+                                </div>
+                                <div class="card-footer ms-auto">
+                                    <small class="text-secondary">Miri Müller</small>
+                                </div>
+                </div>
+            </div>
+            
+        </div>
+            
+    </div>
+    <!-- g -->
+</div>
+</main>
+
+
+
+            
---
+++ php/Uebungen/tag15/views/register.inc.php
@@ -0,0 +1,8 @@
+<header  class="jumbotron bg-image px-5 py-3 position-relative" style="background :linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 70%),url('images/bg3.jpg'); height: calc(100vh - 500px); background-size:100% 160%; background-position:100%; background-repeat: no-repeat;">
+    <p class="display-5 text-light position-absolute top-50">Register</p>
+</header>
+
+<div class="bg-main p-5">
+<main>
+
+</main>
